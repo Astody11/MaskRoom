dbd5472c1245012072ac33c6a83b23a7e5e79374
Inés <astody2@gmail.com>
2026-01-30 20:46:59 +0100

    Added some code

--- Assets/Scripts/RoomSectionMovement.cs
+++ Assets/Scripts/RoomSectionMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RoomSectionMovement : MonoBehaviour
 {
+
+    public GameObject roomSection;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,19 @@ public class RoomSectionMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            RoomRotation(-90);
+        }
+
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            RoomRotation(90);
+        }
+    }
+
+    void RoomRotation(int angle)
+    {
+        roomSection.transform.Rotate(0, angle, 0);
     }
 }
