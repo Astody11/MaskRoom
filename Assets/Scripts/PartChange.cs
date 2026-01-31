92129ddc0757d03f78f1794f3e5c0d6d99122d25
alexgnz03 <alexgnz03@gmail.com>
2026-01-31 13:03:40 +0000

    Se han añadido las funciones de locked

--- Assets/Scripts/PartChange.cs
+++ Assets/Scripts/PartChange.cs
@@ -11,39 +11,51 @@ public class PartChange : MonoBehaviour
     [SerializeField] private Transform midEmpty;
     [SerializeField] private Transform downEmpty;
 
-    public void MaskButtonAction(string color)
+    public void MaskButtonAction(PartMask currentMask)
     {
+        mask = currentMask;
+
         if (mask == null)
         {
             Debug.LogError("MASK COMPONENT ES NULL", this);
             return;
         }
 
-        switch (color)
-        {
-            case "red":
-                ChangePart(mask.upPart, mask.midPart, mask.downPart);
-                break;
-        }
+        
+        ChangePart(mask.upPart, mask.midPart, mask.downPart);
+        
     }
 
     public void ChangePart(GameObject upPart, GameObject midPart, GameObject downPart)
     {
-        foreach (Transform child in upEmpty)
+        if (upEmpty.GetComponent<EachSectionManager>().isLocked == false)
         {
-            Destroy(child.gameObject);
-        }
-        foreach (Transform child in midEmpty)
-        {
-            Destroy(child.gameObject);
-        }
-        foreach (Transform child in downEmpty)
-        {
-            Destroy(child.gameObject);
-        }
+            foreach (Transform child in upEmpty)
+            {
+                Destroy(child.gameObject);
+            }
 
-        Instantiate(upPart, upEmpty).transform.localPosition = Vector3.zero;
-        Instantiate(midPart, midEmpty).transform.localPosition = Vector3.zero;
-        Instantiate(downPart, downEmpty).transform.localPosition = Vector3.zero;
+            Instantiate(upPart, upEmpty).transform.localPosition = Vector3.zero;
+
+        }
+        if (midEmpty.GetComponent<EachSectionManager>().isLocked == false)
+        {
+            foreach (Transform child in midEmpty)
+            {
+                Destroy(child.gameObject);
+            }
+
+            Instantiate(midPart, midEmpty).transform.localPosition = Vector3.zero;
+        }
+        if (downEmpty.GetComponent<EachSectionManager>().isLocked == false)
+        {
+            foreach (Transform child in downEmpty)
+            {
+                Destroy(child.gameObject);
+            }
+
+            Instantiate(downPart, downEmpty).transform.localPosition = Vector3.zero;
+        }
+        
     }
 }
